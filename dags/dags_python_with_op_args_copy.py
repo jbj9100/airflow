9db9adf2262a37334ed1e0412aca799583bcba63
DESKTOP-UCG2H7Q\정병재 <wjdqudwo0@naver.com>
2024-11-12 16:39:52 +0900

    copy name change

--- dags/dags_python_with_op_args_copy.py
+++ dags/dags_python_with_op_args_copy.py
@@ -6,7 +6,7 @@ from airflow.decorators import task
 from common.common_func import regist
 
 with DAG(
-    dag_id="dags_python_with_op_args",
+    dag_id="dags_python_with_op_args_copy",
     schedule="30 6 * * *",
     start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
     catchup=False
