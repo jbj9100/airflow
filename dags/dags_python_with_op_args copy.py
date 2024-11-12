2b683328ca1fa65896d2d0de3891a9f2e253bfd0
DESKTOP-UCG2H7Q\정병재 <wjdqudwo0@naver.com>
2024-11-12 16:37:45 +0900

    copy add

---
+++ dags/dags_python_with_op_args copy.py
@@ -0,0 +1,18 @@
+from airflow import DAG
+import pendulum
+import datetime
+from airflow.operators.python import PythonOperator
+from airflow.decorators import task
+from common.common_func import regist
+
+with DAG(
+    dag_id="dags_python_with_op_args",
+    schedule="30 6 * * *",
+    start_date=pendulum.datetime(2024, 1, 1, tz="Asia/Seoul"),
+    catchup=False
+) as dag:
+
+    @task(task_id=regist_t1)
+    def task_regist():
+        return regist('hjkim','man','kr','seoul')
+    regist_t1 = task_regist()
